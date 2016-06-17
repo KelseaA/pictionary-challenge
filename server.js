0113c905085034715e4474af60b4df42d5d48677
Kelsea <andersonkelsea@gmail.com>
2016-06-16 20:26:30 -0400

    Pictionary game beginning - added sign in modal

---
+++ server.js
@@ -0,0 +1,15 @@
+var http = require('http');
+var express = require('express');
+var socket_io = require('socket.io');
+
+var app = express();
+app.use(express.static('public'));
+
+var server = http.Server(app);
+var io = socket_io(server);
+
+// io.on('connection', function(socket){
+	
+// });
+
+server.listen(8080);
