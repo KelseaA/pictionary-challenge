d1e0967828ee7111e1073bebf7eb5a1e799994cc
Kelsea <andersonkelsea@gmail.com>
2016-06-18 13:37:39 -0400

    Finished sign in, started main game

---
+++ public/js/main.js
@@ -0,0 +1,50 @@
+var socket = io();
+var selectedAvatar = "avatars/avatar1.png";
+
+var User = function(firstName, lastInitial, avatar){
+	this.firstName = firstName,
+	this.lastInitial = lastInitial,
+	this.avatar = avatar
+}
+
+var updateUsers = function(users){
+	console.log(users);
+}
+
+socket.on("updateUsers", updateUsers);
+
+//Select avatar icon on click
+$(".avatar-icons").on("click", function(){
+	$(".avatar-icons").removeClass("selected");
+	$(this).addClass("selected");
+	console.log("avatar click");
+
+	//Assign avatar image choice to user on avatar click
+	selectedAvatar = $(".selected").attr("src");
+	console.log(selectedAvatar);
+
+	$(".chosen-avatar").html('<img class="avatar-icons" src="' + selectedAvatar + '">');
+});
+
+// Create a new user with name selections on submit click
+$(".user-submit").on("click", function(){
+	//Assign value of first and last name inputs to variables
+	var firstName = $("#first-name").val();
+	console.log(firstName);
+	var lastInitial = $("#last-initial").val();
+	console.log(lastInitial);
+	//If no name is entered or avatar chosen, alert user to make a selection
+	if(firstName == "" || lastInitial == ""){
+		//how to do this ^^ for avatar?
+		alert("Please enter a first name and last initial");
+	}
+	else{
+		//Add username to div on main game 
+		$(".username").html("<h2>" + firstName + " " + lastInitial + "</h2");
+		var myUser = new User(firstName, lastInitial, selectedAvatar);
+		socket.emit("newUser", myUser);
+	}
+
+	//Hide sign in page and show main page
+	$(".sign-in-container").hide();
+});
--- server.js
+++ server.js
@@ -8,8 +8,22 @@ app.use(express.static('public'));
 var server = http.Server(app);
 var io = socket_io(server);
 
-// io.on('connection', function(socket){
-	
-// });
+//pictionary application variables
+var users = [];
+
+io.on('connection', function(socket){
+	socket.on("newUser", function(newUser){
+		console.log(newUser);
+		console.log(socket.id);
+		users[socket.id] = newUser;
+		socket.emit("updateUsers", users);
+	});
+	socket.on("disconnect", function(){
+		console.log(socket.id);
+		delete users[socket.id];
+		console.log(users);
+		socket.emit("updateUsers", users);
+	});
+});
 
 server.listen(8080);
